71418cd5d86d01c56057d10e018b0ee05ec2f02b
joeyslalom <joeyt@slalom.com>
2020-02-17 18:43:14 -0800

    Use git-version plugin (#2)
    The project version is now determined by git.  That is, Gradle's project version (`./gradlew printVersion`), is the same as the output of `git describe`.
    
    Use GitHub releases or `git tag` to create a new version at a specific gitsha.
    Otherwise, the version is calculated by the number of commits away from the previous version, plus the current gitsha:
    ```
    $ git describe
    v1.0.0-1-gfd83bed
    $ ./gradlew :printVersion
    > Task :printVersion
    v1.0.0-1-gfd83bed
    
    ```

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
+import com.palantir.gradle.gitversion.VersionDetails
+
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
     kotlin("jvm") version "1.3.61" apply false
     kotlin("plugin.spring") version "1.3.61" apply false
+    id("com.palantir.git-version") version "0.12.2" apply false
 }
 
-group = "github.chickenbane"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    apply(plugin = "com.palantir.git-version")
+
+    fun getVersionDetails(): VersionDetails = (extra["versionDetails"] as groovy.lang.Closure<*>)() as VersionDetails
+
+    group = "github.chickenbane"
+    version= getVersionDetails().version
+}
